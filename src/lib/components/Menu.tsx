7ca78e256da5b601408039b900a0d5f06e6ba194
SamanthaJeanneb <sbrown27@oswego.edu>
2025-04-13 08:42:50 -0400

    Commit

--- src/lib/components/Menu.tsx
+++ src/lib/components/Menu.tsx
@@ -1,21 +1,36 @@
+import { useEffect } from "react";
+
 type Props = {
-    setScreen: React.Dispatch<React.SetStateAction<'menu' | 'game' | 'score'>>;
-  };
-  
-  const Menu = ({ setScreen }: Props) => {
-    return (
-      <div className="menu">
-        <img src="title.png" alt="Game Title" className="title-image" />
-        <div className="button-container">
-          <img
-            src="play.png"
-            alt="Play"
-            className="play-image"
-            onClick={() => setScreen('game')}
-          />
-        </div>
+  setScreen: React.Dispatch<React.SetStateAction<"menu" | "game" | "score">>;
+};
+
+const Menu = ({ setScreen }: Props) => {
+  useEffect(() => {
+    const handleKeyPress = (event: KeyboardEvent) => {
+      if (event.key === "s") {
+        setScreen("game");
+      }
+    };
+
+    window.addEventListener("keydown", handleKeyPress);
+    return () => {
+      window.removeEventListener("keydown", handleKeyPress);
+    };
+  }, [setScreen]);
+
+  return (
+    <div className="menu">
+      <img src="title.png" alt="Game Title" className="title-image" />
+      <div className="button-container">
+        <img
+          src="play.png"
+          alt="Play"
+          className="play-image"
+          onClick={() => setScreen("game")}
+        />
       </div>
-    );
-  };
-  
-  export default Menu;
+    </div>
+  );
+};
+
+export default Menu;
