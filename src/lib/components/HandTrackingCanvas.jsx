835dd6eac460317c29319a59f5643650ad850a19
SamanthaJeanneb <sbrown27@oswego.edu>
2025-04-13 08:46:30 -0400

    Update HandTrackingCanvas.jsx

--- src/lib/components/HandTrackingCanvas.jsx
+++ src/lib/components/HandTrackingCanvas.jsx
@@ -1,11 +1,10 @@
-import React, { useEffect, useRef, useContext } from "react";
+import React, { useEffect, useContext } from "react";
 import * as tf from "@tensorflow/tfjs";
 import * as handpose from "@tensorflow-models/handpose";
 import "@tensorflow/tfjs-backend-webgl";
 import { FingerStateContext } from "../context/FingerStateContext";
 
 const HandTrackingCanvas = () => {
-  const videoRef = useRef(null);
   const { setFingerInRange } = useContext(FingerStateContext);
 
   const processHandData = (predictions) => {
@@ -46,7 +45,10 @@ const HandTrackingCanvas = () => {
     const loadModelAndTrackHands = async () => {
       await tf.setBackend("webgl");
       const model = await handpose.load();
-      const video = videoRef.current;
+
+      const video = document.createElement("video");
+      video.style.display = "none"; 
+      document.body.appendChild(video); 
 
       const startVideo = async () => {
         try {
@@ -54,7 +56,7 @@ const HandTrackingCanvas = () => {
             video: true,
           });
           video.srcObject = stream;
-          video.play();
+          await video.play();
           console.log("Camera started successfully.");
         } catch (error) {
           console.error("Error accessing the camera:", error);
@@ -73,6 +75,12 @@ const HandTrackingCanvas = () => {
 
       await startVideo();
       detectHands();
+
+      return () => {
+        const tracks = video.srcObject?.getTracks();
+        tracks?.forEach((track) => track.stop());
+        video.remove();
+      };
     };
 
     loadModelAndTrackHands();
